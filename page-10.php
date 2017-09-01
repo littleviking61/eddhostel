e321a297993c5d9a8b75d896e86b644b8e258b04
Baptiste Régné <baptiste.regne@gmail.com>
2017-09-01 18:02:58 +0200

    texte home + EDD

--- page-10.php
+++ page-10.php
@@ -1,35 +1,29 @@
 <?php get_header(); ?>
 
+
 <?php /* HOME */ ?>
 
 	<main role="main" aria-label="Content">
+
+		<!-- booking -->
+		<div class="booking top"></div>
+		
 		<!-- section -->
 		<section class="container">
 
-			<h1 class="page-title"><?php the_title(); ?></h1>
+			<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+				
+				<div class="introduction row">
+					<div class="accroche">
+						<i class="eddicon-line"></i>
+						<h2><?= get_field('titre_daccroche') ?><span class="big color">Edd</span></h2>
+					</div>
+					<div class="texte">
+						<p><span class="big color">Edd</span> <span class="big">Hostel</span> <?= get_field('texte_daccroche') ?></p>
+					</div>
+				</div>
 
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
-
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-				Home
-			</article>
-			<!-- /article -->
-
-		<?php endwhile; ?>
-
-		<?php else: ?>
-
-			<!-- article -->
-			<article>
-
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
-
-			</article>
-			<!-- /article -->
-
-		<?php endif; ?>
+			<?php endwhile; endif; ?>
 
 		</section>
 		<!-- /section -->
