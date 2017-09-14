28b9563a5eaf3b3d7414837a8b080c844469c6e9
Baptiste Régné <baptiste.regne@gmail.com>
2017-09-14 22:52:18 +0200

    little hover picto correction

--- loop.php
+++ loop.php
@@ -9,40 +9,42 @@ query_posts( $args ); ?>
 		<!-- article -->
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-					<?php the_post_thumbnail('medium'); // Declare pixel size you need inside the array ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+				<!-- post thumbnail -->
+				<?php if ( has_post_thumbnail()) :  ?>
+					<div class="thumbnail">
+						<?php the_post_thumbnail('medium');  ?>
+					</div>
+				<?php endif; ?>
+				<!-- /post thumbnail -->
 
-			<div class="content">
-				<!-- categorie -->
-				<ul class="main-tags">
-					<?php $main_tags = get_field('main_tag'); ?>
-					<?php foreach ($main_tags as $main_tag): $main_tag = get_term($main_tag); ?>
-						<li><a href="<?= get_term_link($main_tag->term_id); ?>"><?= $main_tag->name; ?></a></li>
-					<?php endforeach ?>
-				</ul>
-				<!-- /categorie -->
+				<div class="content">
+					<!-- categorie -->
+					<ul class="main-tags">
+						<?php $main_tags = get_field('main_tag'); ?>
+						<?php foreach ($main_tags as $main_tag): $main_tag = get_term($main_tag); ?>
+							<li><?= $main_tag->name; ?></li>
+						<?php endforeach ?>
+					</ul>
+					<!-- /categorie -->
 
-				<!-- post title -->
-				<h4>
-					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-				</h4>
-				<!-- /post title -->
+					<!-- post title -->
+					<h4>
+						<?php the_title(); ?>
+					</h4>
+					<!-- /post title -->
 
-				<!-- post details -->
-				<div class="entry-meta">
-					<span class="date">
-						<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
-							<?php the_time('d/m/y'); ?>
-						</time>
-					</span>
-					<span class="author"><?php the_author(); ?></span>
+					<!-- post details -->
+					<div class="entry-meta">
+						<span class="date">
+							<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
+								<?php the_time('d/m/y'); ?>
+							</time>
+						</span>
+						<span class="author"><?php the_author(); ?></span>
+					</div>
 				</div>
-			</div>
+			</a>
 
 		</article>
 		<!-- /article -->
